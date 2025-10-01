36fecbb4b2a6934670d0477368cfaca0cddb3be1
Ghofran Malkat <gh.malket@gmail.com>
2025-10-01 12:21:37 +0200

    store the userId in secure storage

--- lib/utils/auth_storage.dart
+++ lib/utils/auth_storage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class AuthStorage {
   final _storage = const FlutterSecureStorage();
   static const _tokenKey = 'auth_token';
+  static const _userId = 'userId';
 
   Future<String?> getToken() async {
     return await _storage.read(key: _tokenKey);
@@ -14,6 +15,14 @@ class AuthStorage {
 
   Future<void> deleteToken() async{
     await _storage.delete(key: _tokenKey);
+  }
+
+  Future<String?> getUserId() async {
+    return await _storage.read(key: _userId);
+  }
+
+  Future<void> saveUserId(userId) async{
+    await _storage.write(key: _userId, value: userId);
   }
 
 }
