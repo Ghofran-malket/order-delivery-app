1f568f92d71aedcf812b11e6f8b4b57ec81bb1e6
Ghofran Malkat <gh.malket@gmail.com>
2025-10-10 11:57:21 +0200

    Call api to get the taken orders by genie

---
+++ lib/services/order_api_services.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:algenie/data/models/order_model.dart';
+import 'package:algenie/utils/auth_storage.dart';
+import 'package:http/http.dart' as http;
+
+class OrderApiService {
+  final String baseUrl = "http://192.168.1.89:3000/api/";
+  final AuthStorage storage = AuthStorage();
+
+  //get orders
+  Stream<List<Order>> getTakenOrders() async* {
+    final genieId = await storage.getUserId();
+    final response = await http.get(
+      Uri.parse('${baseUrl}orders/getTakenOrders/?genieId=$genieId'),
+      headers: {'Content-Type': 'application/json'}
+    );
+    if(response.statusCode == 200){
+      final List<dynamic> data = jsonDecode(response.body);
+      final List<Order> orders = data.map((e) => Order.fromJson(e)).toList();
+      yield orders;
+    } else {
+      yield [];
+    }
+
+  }
+
+
+}
